61a5ea03ffbb957f3769e8fee822dd6d3d74f4b2
Vyacheslav Ermakov <slavaskainet@gmail.com>
2019-10-04 00:17:37 +0300

    add skills

--- src/components/cards/cards.svelte
+++ src/components/cards/cards.svelte
@@ -1,19 +1,31 @@
 <script>
   import Card from "./card/card.svelte";
+  import FireIcon from "../icons/fire.svelte";
+  import ShieldIcon from "../icons/shield.svelte";
 
   const cards = [1, 2, 3];
 </script>
 
 <style lang="scss">
-    .cards {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
+  .cards {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  :global(.green) {
+    background: green;
+  }
 </style>
 
 <div class="cards">
-  {#each cards as item}
-    <Card />
-  {/each}
+  <Card>
+    <FireIcon />
+  </Card>
+  <Card className="green">
+    <ShieldIcon />
+  </Card>
+  <Card>
+    <FireIcon />
+  </Card>
 </div>
